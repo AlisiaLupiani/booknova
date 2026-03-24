1190f585c25d98480573664476d3c3bcbe8afe45
Rachid <rachid.nhachi@student.univaq.it>
2026-03-24 12:25:09 +0100

    creazione di file DAO

--- include/model/Role.php
+++ include/model/Role.php
@@ -1,23 +1,25 @@
 <?php
 
-class Ruolo{
+class Ruolo {
 
     private ?int $id;
     private ?string $role;
 
-
-    public function public function __construct() {
+    public function __construct() {
         $this->id = null;
         $this->role = null;
     }
 
-//getter
-    public function getId(): ?int {return $this->id;}
-    public function getRuolo(): ?string {return $this->role;}
+    // Getter
+    public function getId(): ?int { return $this->id; }
+    public function getRuolo(): ?string { return $this->role; }
 
-//setter
-    public function setId(?int $id): void {$this->id = $id;}
-    public function setRuolo(?string $role): void { $this->role = $role;}
+    // Setter
+    public function setId(?int $id): void { $this->id = $id; }
+    public function setRuolo(?string $role): void { $this->role = $role; }
+
+    // Metodo toString aggiunto
+    public function toString(): string {
+        return $this->role ?? '';
+    }
 }
-
-
