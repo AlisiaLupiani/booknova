0450556c5e8b485d38b8ae24e0af24747cb55a92
AlisiaLupiani <aulisia232306@gmail.com>
2026-03-26 21:07:58 +0100

    ciccia

---
+++ php/visualizza_ordini/visualizza_ordine.php
@@ -0,0 +1,13 @@
+<?php
+
+
+if(!isset($_SESSION['auth'])) {
+    header("Location: login.php?reference=visualizza_ordine.php");
+    exit;
+}
+
+
+
+$body_page = new Template("html/visualizza_ordini/visualizza_ordini.html");
+
+?>
